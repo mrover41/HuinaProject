2aa58ff470e558a062537fc653a1b593401c5d71
mrover41 <mrover41@users.noreply.github.com>
2026-09-10 19:37:29 +0300

    hook and shift

--- Assets/_Game/Scripts/Player/ModuleSystem/Modules/Hook.cs
+++ Assets/_Game/Scripts/Player/ModuleSystem/Modules/Hook.cs
@@ -8,33 +8,54 @@ public class Hook : ModuleBase {
 
     private Vector3 pos;
     private Rigidbody _rb;
+    private LineRenderer lineRenderer;
 
     [SerializeField] private float maxLenght = 200;
-    [SerializeField] private float speed = 1000;
+    [SerializeField] private float speed = 2500;
     [SerializeField] private LayerMask hookLayer = 1 << 6;
     [SerializeField] private float maxDistance = 50;
 
     public override void OnEnable(Player pl) {
         _rb = pl.gameObject.GetComponent<Rigidbody>();
+        lineRenderer = pl.gameObject.AddComponent<LineRenderer>();
+
+        lineRenderer.startWidth = 0.1f;
+        lineRenderer.endWidth = 0.1f;
+        lineRenderer.positionCount = 2;
+        lineRenderer.enabled = false;
+
+        Material lineMaterial = new Material(Shader.Find("Sprites/Default"));
+        lineMaterial.color = Color.black;
+        lineRenderer.material = lineMaterial;
     }
 
     public override void OnUpdate() {
-        if (Input.GetKeyDown(KeyCode.F))
+        if (Input.GetKeyDown(KeyCode.F)) {
+            lineRenderer.enabled = true;
             hooking = true;
+        }
+
 
         if (Input.GetKeyUp(KeyCode.F)) {
             hooking = false;
             isFinded = false;
-        }
 
+            lineRenderer.enabled = false;
+        }
+    }
+
+    public override void OnFixedUpdate() {
         if (hooking) {
             Transform c = Camera.main.gameObject.transform;
-            if (!isFinded && Physics.Raycast(c.position, c.forward, out RaycastHit hit, maxLenght, hookLayer)) {
+            if (!isFinded && Physics.Raycast(c.position, c.forward, out RaycastHit hit, maxLenght) && hit.collider.gameObject.CompareTag("Hook")) {
                 pos = hit.point;
                 isFinded = true;
-            }
 
-            _rb.AddForce((pos - c.position).normalized * speed * Time.deltaTime);
+                lineRenderer.SetPosition(1, hit.point);
+            } else if (isFinded) {
+                lineRenderer.SetPosition(0, c.position + Vector3.down);
+                _rb.AddForce((pos - c.position).normalized * speed * Time.fixedDeltaTime);
+            }
         }
     }
 }
--- Assets/_Game/Scripts/Player/ModuleSystem/Modules/PlayerMovment.cs
+++ Assets/_Game/Scripts/Player/ModuleSystem/Modules/PlayerMovment.cs
@@ -4,26 +4,58 @@ using UnityEngine;
 public class PlayerMovment : ModuleBase {
     private Rigidbody _rb;
 
+    private float slashT = 0;
+    private Vector3 slashDirection;
+
+    private Vector3 input;
+    private Vector3 direction;
+
     [SerializeField] private float maxSpeed = 10f;
     [SerializeField] private float force = 15f;
     [SerializeField] private float deadZone = 0.1f; //only for joysticks
     [SerializeField] private float defaultDamping = 5;
     [SerializeField] private float damping = 0.5f;
-    [SerializeField] private float jumpSpeed = 6;
-    [SerializeField] private float slashSpeed = 10f;
+    [SerializeField] private float jumpSpeed = 10;
+    [SerializeField] private float slashSpeed = 10;
+    [SerializeField] private float slashTime = 0.3f;
+    [SerializeField] private float slashCooldown = 5;
     [SerializeField] private LayerMask groundLayer = 1 << 3;
 
-    public bool grounded = true;
+    public bool grounded {get; private set;} = true;
+    public bool isEnabled = true;
 
     public override void OnEnable(Player pl) {
         _rb = pl.gameObject.GetComponent<Rigidbody>();
     }
 
-    public override void OnFixedUpdate() {
+    public override void OnUpdate() {
+        if (!isEnabled) return;
+
         grounded = isGrounded();
 
-        Vector3 input = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-        Vector3 direction = (player.gameObject.transform.right * input.x + player.gameObject.transform.forward * input.z).normalized * force * Time.fixedDeltaTime;
+        UpdateMoving();
+        UpdateInput();        
+    }
+
+
+    private void UpdateInput() {
+        if (Input.GetKeyDown(KeyCode.Space) && grounded) { 
+            _rb.linearVelocity += new Vector3(0, jumpSpeed, 0);
+        } if (Input.GetKeyDown(KeyCode.LeftShift) && slashT + slashTime <= Time.time) {
+            slashT = Time.time;
+            slashDirection = direction.normalized * slashSpeed;
+            if (slashDirection.magnitude == 0) slashDirection = player.gameObject.transform.forward;
+        }
+    }
+
+    private void UpdateMoving() {
+        input = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        direction = (player.gameObject.transform.right * input.x + player.gameObject.transform.forward * input.z).normalized * force * Time.deltaTime;
+
+        if (slashT + slashTime >= Time.time) {
+            _rb.MovePosition(player.gameObject.transform.position + (slashDirection * slashSpeed) * Time.deltaTime);
+            return;
+        }
 
         if (input.magnitude > deadZone && grounded) {
             _rb.linearDamping = damping;
@@ -33,12 +65,6 @@ public class PlayerMovment : ModuleBase {
         } else {
             _rb.linearDamping = 0;
         }
-
-        if (Input.GetKey(KeyCode.Space) && grounded) { 
-            _rb.linearVelocity += new Vector3(0, jumpSpeed, 0);
-            grounded = false;
-        }
-
     }
 
     private bool isGrounded() {
